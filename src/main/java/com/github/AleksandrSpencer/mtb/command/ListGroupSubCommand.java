01d1015f6c749b966f34654d658ee8c55902f8c0
spencer <alexandr.s.spencer@gmail.com>
2023-08-02 17:32:48 +0300

    MTB-6
    added the ability to delete group subscription

--- src/main/java/com/github/AleksandrSpencer/mtb/command/ListGroupSubCommand.java
+++ src/main/java/com/github/AleksandrSpencer/mtb/command/ListGroupSubCommand.java
@@ -4,6 +4,7 @@ import com.github.AleksandrSpencer.mtb.repository.entity.GroupSub;
 import com.github.AleksandrSpencer.mtb.repository.entity.TelegramUser;
 import com.github.AleksandrSpencer.mtb.service.SendBotMessageService;
 import com.github.AleksandrSpencer.mtb.service.TelegramUserService;
+import org.springframework.util.CollectionUtils;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 import javax.ws.rs.NotFoundException;
@@ -31,12 +32,18 @@ public class ListGroupSubCommand implements Command{
         TelegramUser telegramUser = telegramUserService.findByChatId(getChatId(update))
                 .orElseThrow(NotFoundException::new);
 
-        String message = "Я нашел все подписки на группы: \n\n";
-        String collectedGroups = telegramUser.getGroupSubs().stream()
-                .map(it -> "Группа: " + it.getTitle() + " , ID = " + it.getId() + " \n")
-                .collect(Collectors.joining());
-
-        sendBotMessageService.sendMessage(telegramUser.getChatId(), message + collectedGroups);
+        String message;
+        if(CollectionUtils.isEmpty(telegramUser.getGroupSubs())) {
+            message = "Пока нет подписок на группы. Чтобы добавить подписку напиши /addgroupsub ";
+        } else {
+            String collectedGroups = telegramUser.getGroupSubs().stream()
+                    .map(it -> "Группа: " + it.getTitle() + " , ID = " + it.getId() + " \n")
+                    .collect(Collectors.joining());
+            message =  String.format("Я нашел все подписки на группы: \n\n %s", collectedGroups);
+        }
+        sendBotMessageService.sendMessage(telegramUser.getChatId(), message);
     }
+
+
 
 }
